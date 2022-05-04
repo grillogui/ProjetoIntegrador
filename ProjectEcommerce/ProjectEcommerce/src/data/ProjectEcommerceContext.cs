5e44e1e51febb703dcb40388396cc1603197dfe9
karolcoliveira <oliveiraakarol@hotmail.com>
2022-05-04 14:58:44 -0300

    @karolcoliveira [Task 3: Criação de contexto] {04/05/2022 15:00}

---
+++ ProjectEcommerce/ProjectEcommerce/src/data/ProjectEcommerceContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectEcommerce.src.models;
+
+namespace ProjectEcommerce.src.data
+{
+    public class ProjectEcommerceContext : DbContext 
+    {
+            public DbSet<UserModel> Users { get; set; }
+
+            public DbSet<ProductModel> Products { get; set; }
+
+            public DbSet<PurchaseModel> Purchases { get; set; }
+
+            public ProjectEcommerceContext(DbContextOptions<ProjectEcommerceContext> opt) : base(opt)
+            {
+            }
+      
+    }
+}
