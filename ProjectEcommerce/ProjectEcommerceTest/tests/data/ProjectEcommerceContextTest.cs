bc33b43fb408343bafe0f38293540085305cecdd
italopenha <italopenha77@outlook.com>
2022-05-04 16:30:46 -0300

    @italopenha [Task 3: Efetuando teste unitário para um banco em memória] {04/05/2022 16:29}

---
+++ ProjectEcommerce/ProjectEcommerceTest/tests/data/ProjectEcommerceContextTest.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEcommerce.src.data;
+using ProjectEcommerce.src.models;
+using System.Linq;
+
+namespace ProjectEcommerceTest.tests.data
+{
+    [TestClass]
+    public class ProjectEcommerceContextTest
+    {
+        private ProjectEcommerceContext _context;
+
+        [TestInitialize]
+        public void init()
+        {
+            var opt = new DbContextOptionsBuilder<ProjectEcommerceContext>()
+            .UseInMemoryDatabase(databaseName: "db_projectecommerce")
+            .Options;
+
+            _context = new ProjectEcommerceContext(opt);
+        }
+
+        [TestMethod]
+        public void InsertNewUserIntoDatabaseReturnUser()
+        {
+            UserModel user = new UserModel();
+		
+		    user.Email = "italo@email.com";
+            user.Name = "Ítalo Penha";
+            user.Password = "134652";
+            user.Type = "Consumidor";
+		    user.Address = "Rua Itália, nº 53";
+
+            _context.Users.Add(user); // Adicionando usuário
+
+            _context.SaveChanges(); // Comita Criação
+
+            Assert.IsNotNull(_context.Users.FirstOrDefault(u => u.Email == "italo@email.com"));
+        }
+    }
+
+}
