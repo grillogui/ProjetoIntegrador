12061be41013a17ff344c31de258cfa0c6af6227
karolcoliveira <oliveiraakarol@hotmail.com>
2022-05-05 10:05:24 -0300

    @karolcoliveira [Task 4: Criação de interfaces e DTO de usuários] {05/05/2022 10:00}

---
+++ ProjectEcommerce/ProjectEcommerce/src/dtos/UserDTO.cs
@@ -0,0 +1,71 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectEcommerce.src.dtos
+{
+    public class UserDTO
+    {
+        /// <summary>
+        /// <para>Resume: Mirror class to add a new user</para>
+        /// <para>Created by: Karol Oliveira and Guilherme Grillo</para>
+        /// <para>Version: 1.0</para>
+        /// <para>Date: 05/05/2022</para>
+        /// </summary>
+
+        public class AddUserDTO
+        {
+            [Required, StringLength(30)]
+            public string Email { get; set; }
+
+            [Required, StringLength(50)]
+            public string Name { get; set; }
+
+            [Required, StringLength(30)]
+            public string Password { get; set; }
+
+            [Required, StringLength(30)]
+            public string Type { get; set; }
+
+            [Required, StringLength(255)]
+            public string Address { get; set; }
+
+            public AddUserDTO(string email, string name, string password, string type, string address)
+            {
+                Email = email;
+                Name = name;
+                Password = password;
+                Type = type;
+                Address = address;
+            }
+        }
+        /// <summary>
+        /// <para>Sumary: Mirror class to update an user</para>
+        /// <para>Created by: Karol Oliveira and Guilherme Grillo</para>
+        /// <para>Version: 1.0</para>
+        /// <para>Date: 05/05/2022</para>
+        /// </summary>
+
+        public class UpdateUserDTO
+        {
+            [Required, StringLength(50)]
+            public string Name { get; set; }
+
+            [Required, StringLength(30)]
+            public string Password { get; set; }
+
+            [Required, StringLength(30)]
+            public string Type { get; set; }
+
+            [Required, StringLength(255)]
+            public string Address { get; set; }
+
+            public UpdateUserDTO(string name, string password, string type, string address)
+            {
+                Name = name;
+                Password = password;
+                Type = type;
+                Address = address;
+            }
+        }
+    }
+}
+
---
+++ ProjectEcommerce/ProjectEcommerce/src/repositories/IUser.cs
@@ -0,0 +1,26 @@
+﻿
+using ProjectEcommerce.src.models;
+using System.Collections.Generic;
+using System.Linq;
+using static ProjectEcommerce.src.dtos.UserDTO;
+
+namespace ProjectEcommerce.src.repositories
+{
+    /// <summary>
+    /// <para>Resume: Interface to represent CRUD actions in users</para>
+    /// <para>Created by: Karol Oliveira and Guilherme Grillo</para>
+    /// <para>Version: 1.0</para>
+    /// <para>Date: 05/05/2022</para>
+    /// </summary>
+
+    public interface IUser
+    {
+        void AddUser(AddUserDTO user);
+        void UpdateUser(UpdateUserDTO user);
+        void DeleteUser(int id);
+        UserModel GetUserById(int id);
+        UserModel GetUserByEmail(string email);
+        List<UserModel> GetUserByName(string name);
+        List<UserModel> GetUserByType(string type);
+    }
+}
