85295ec473a96981fee4d357f48a84c2e501c14c
joce-line <joceline.gutierrez@hotmail.com>
2022-05-05 10:39:02 -0300

    @joce-line [Task 4: Criação de interface do product] {05/05/2022 10:38}

---
+++ ProjectEcommerce/ProjectEcommerce/src/repositories/IProduct.cs
@@ -0,0 +1,17 @@
+﻿using BlogPessoal.src.dtos;
+using ProjectEcommerce.src.models;
+using System.Collections.Generic;
+
+namespace ProjectEcommerce.src.repositories
+{
+    public interface IProduct 
+    {
+        void NewProduct(NewProductDTO post);
+        void UpdateProduct(UpdateProductDTO post);
+        void DeleteProduct(int id);
+        ProductModel GetProductById(int id);
+        List<ProductModel> GetAllProducts();
+        List<ProductModel> GetProductBySearch(string name, string descriptionProduct);
+
+    }
+}
