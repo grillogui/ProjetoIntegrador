314615ea8d6601950cae58fc72fb949f34c3e046
italopenha <italopenha77@outlook.com>
2022-05-05 10:25:26 -0300

    @italopenha [Task 4: Criação de ProductDTO] {05/05/2022 10:25}

---
+++ ProjectEcommerce/ProjectEcommerce/src/dtos/ProductDTO.cs
@@ -0,0 +1,70 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogPessoal.src.dtos
+{
+    /// <summary>
+    /// <para>Resume: Mirror class responsible for create a new product</para>
+    /// <para>Created by: Ítalo Penha</para>
+    /// <para>Version: 1.0</para>
+    /// <para>Date: 05/05/2022</para>
+    /// </summary>
+    public class NewProductDTO
+    {
+        [Required, StringLength(30)]
+        public string Name { get; set; }
+
+        [Required, StringLength(100)]
+        public float Price { get; set; } 
+
+        public string Image { get; set; }
+
+        [Required, StringLength(30)]
+        public string Description { get; set; } 
+
+        [Required]
+        public string Quantity { get; set; } 
+
+        public NewProductDTO(string name, float price, string image, string description, string quantity)
+        {
+            Name = name;
+            Price = price;
+            Image = image;
+            Description = description;
+            Quantity = quantity;
+        }
+    }
+
+    /// <summary>
+    /// <para>Resume: Mirror class responsible for update a new product</para>
+    /// <para>Created by: Ítalo Penha</para>
+    /// <para>Version: 1.0</para>
+    /// <para>Date: 05/05/2022</para>
+    /// </summary>
+
+    public class UpdateProductDTO
+    {
+        
+        [Required, StringLength(30)]
+        public string Name { get; set; }
+
+        [Required, StringLength(100)]
+        public float Price { get; set; } 
+
+        public string Image { get; set; }
+
+        [Required, StringLength(30)]
+        public string Description { get; set; } 
+
+        [Required]
+        public string Quantity { get; set; }
+
+        public UpdateProductDTO(string name, float price, string image, string description, string quantity)
+        {
+            Name = name;
+            Price = price;
+            Image = image;
+            Description = description;
+            Quantity = quantity;
+        }
+    }
+}
