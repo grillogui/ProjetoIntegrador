0779e10f0c8c5ca78aa62ace974312f7552b2843
joce-line <joceline.gutierrez@hotmail.com>
2022-05-09 14:50:14 -0300

    @joce-line [Task 5: Adicionando controlador de Purchase] {09/05/2022 14:50}

--- ProjectEcommerce/ProjectEcommerce/src/controllers/PurchaseController.cs
+++ ProjectEcommerce/ProjectEcommerce/src/controllers/PurchaseController.cs
@@ -1,6 +1,55 @@
-﻿namespace ProjectEcommerce.src.controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectEcommerce.src.dtos;
+using ProjectEcommerce.src.repositories;
+
+namespace ProjectEcommerce.src.controllers
 {
-    public class PurchaseController
+    [ApiController]
+    [Route("api/Purchases")]
+    [Produces("application/json")]
+    public class PurchaseController : ControllerBase
     {
+        #region Attributes
+        private readonly IPurchase _repository;
+        #endregion
+
+        #region Constructors
+        public PurchaseController(IPurchase repository)
+        {
+            _repository = repository;
+        }
+        #endregion
+
+        #region Methods
+
+        [HttpPost]
+        public IActionResult NewPurchase([FromBody] NewPurchaseDTO purchase)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            _repository.NewPurchase(purchase);
+            return Created($"api/Purchases", purchase);
+        }
+
+        [HttpDelete("delete/{idPurchase}")]
+        public IActionResult DeletePurchase([FromRoute] int idPurchase)
+        {
+            _repository.DeletePurchase(idPurchase);
+            return NoContent();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllPurchases()
+        {
+            var list = _repository.GetAllPurchases();
+
+            if (list.Count < 1) return NoContent();
+
+            return Ok(list);
+        }
+
+
+
+        #endregion
     }
 }
