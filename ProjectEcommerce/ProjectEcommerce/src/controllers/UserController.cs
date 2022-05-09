8379e03ac76e97cbf3d7fb0d31017158a4277ffa
italopenha <italopenha77@outlook.com>
2022-05-09 14:02:56 -0300

    @italopenha [Task 5: Criando controladores em usuário] {09/05/2022 14:02}

--- ProjectEcommerce/ProjectEcommerce/src/controllers/UserController.cs
+++ ProjectEcommerce/ProjectEcommerce/src/controllers/UserController.cs
@@ -1,6 +1,65 @@
-﻿namespace ProjectEcommerce.src.controllers
+﻿using ProjectEcommerce.src.dtos;
+using ProjectEcommerce.src.repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectEcommerce.src.controllers
 {
-    public class UserController
+    [ApiController]
+    [Route("api/Users")]
+    [Produces("application/json")]
+
+    public class UserController : ControllerBase
     {
+    #region Atributes
+
+    private readonly IUser _repository;
+
+    #endregion
+
+    #region Constructors
+
+    public UserController(IUser repository)
+    {
+        _repository = repository;
     }
-}
+    
+
+    #endregion
+
+    #region Methods
+
+    [HttpPost]
+    public IActionResult AddUser([FromBody] AddUserDTO user)
+    {
+        if(!ModelState.IsValid) return BadRequest();
+
+        _repository.AddUser(user);
+
+        return Created($"api/Users/{user.Email}", user);
+    }
+
+    [HttpGet("email/{emailUser}")]
+    public IActionResult GetUserByEmail([FromRoute] string emailUser)
+    {
+        var user = _repository.GetUserByEmail(emailUser);
+
+        if (user == null) return NotFound();
+
+        return Ok(user);
+    }
+
+    [HttpGet("id/{idUser}")]
+    public IActionResult GetUserById([FromRoute] int idUser)
+    {
+        var user = _repository.GetUserById(idUser);
+
+        if (user == null) return NotFound();
+
+        return Ok(user);
+    }
+
+
+
+
+    #endregion
+}}
