d7d69d2472c70377598728cc1fa40c20d1445ad1
MatheusBrazolin <matheusbrazolin013@gmail.com>
2022-05-11 16:47:09 -0300

    @MatheusBrazolin [Task 6:Criação do DTO de autentificação] {11/05/2022 16:45}

---
+++ ProjectEcommerce/ProjectEcommerce/src/dtos/AuthorizationDTO.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogPessoal.src.dtos
+{
+    public class AuthenticateDTO
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public AuthenticateDTO(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+    }
+    public class AuthorizationDTO
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public TypeUser Type { get; set; }
+        public string Token { get; set; }
+        public AuthorizationDTO(int id, string email, TypeUser type, string token)
+        {
+            Id = id;
+            Email = email;
+            Type = type;
+            Token = token;
+        }
+    }
+}
