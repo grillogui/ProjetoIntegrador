4d990d977d8a6436e807a5c4998633d3f72a64db
karolcoliveira <oliveiraakarol@hotmail.com>
2022-05-11 18:14:48 -0300

    @karolcoliveira [Task 6: Inclusão de parâmetros em Start up] {11/05/2022 18:14}

--- ProjectEcommerce/ProjectEcommerce/Startup.cs
+++ ProjectEcommerce/ProjectEcommerce/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -6,12 +7,16 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using ProjectEcommerce.src.data;
 using ProjectEcommerce.src.repositories;
 using ProjectEcommerce.src.repositories.implements;
+using ProjectEcommerce.src.services;
+using ProjectEcommerce.src.services.implements;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ProjectEcommerce
@@ -28,13 +33,7 @@ namespace ProjectEcommerce
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Contexto
-            IConfigurationRoot config = new ConfigurationBuilder()
-                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
-                .AddJsonFile("appsettings.json")
-                .Build();
-
-            services.AddDbContext<ProjectEcommerceContext>(opt => opt.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<ProjectEcommerceContext>(opt => opt.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
 
             // Repositories
             services.AddScoped<IUser, UserImplements>();
@@ -44,7 +43,33 @@ namespace ProjectEcommerce
             // Controllers
             services.AddCors();
             services.AddControllers();
+
+
+            // Configuração de Serviços
+
+            services.AddScoped<IAuthentication, AuthenticationServices>();
+
+            // Configuração do Token Autenticação JWTBearer
+
+            var key = Encoding.ASCII.GetBytes(Configuration["Settings:Secret"]);
+            services.AddAuthentication(a =>
+            {
+                a.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                a.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(b =>
+            {
+                 b.RequireHttpsMetadata = false;
+                 b.SaveToken = true;
+                b.TokenValidationParameters = new TokenValidationParameters
+             {
+                     ValidateIssuerSigningKey = true,
+                     IssuerSigningKey = new SymmetricSecurityKey(key),
+                       ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+}           );
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ProjectEcommerceContext context)
@@ -62,10 +87,15 @@ namespace ProjectEcommerce
             .AllowAnyOrigin()
             .AllowAnyMethod()
             .AllowAnyHeader());
+
+            // Autenticação e Autorização
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+                {
+                    endpoints.MapControllers();
+                });
         }
     }
 }
