b485e89e3f6fedfff56d54f1b7099ee42bd98d93
karolcoliveira <oliveiraakarol@hotmail.com>
2022-05-11 15:25:33 -0300

    @karolcoliveira [Task: Criação Enum e utilities] {11/05/2022 15:25}

---
+++ ProjectEcommerce/ProjectEcommerce/src/utilities/Enum.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ProjectEcommerce.src.utilities
+{
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum TypeUser
+    {
+            REGULAR,
+            VULNERABILITY,
+            ADMINISTRATOR
+    }
+}
