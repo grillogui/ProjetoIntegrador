b035a4fe8bb39efcfb13c22d539493b0e47d8d3a
joce-line <joceline.gutierrez@hotmail.com>
2022-05-12 14:59:05 -0300

    @joce-line [Task 6: Adicionando controlador de autenticação] {12/05/2022 14:58}

---
+++ ProjectEcommerce/ProjectEcommerce/src/controllers/AuthenticationController.cs
@@ -0,0 +1,52 @@
+﻿using BlogPessoal.src.dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProjectEcommerce.src.services;
+using System;
+using System.Threading.Tasks;
+
+namespace ProjectEcommerce.src.controllers
+{
+    /// <summary>
+    /// <para>Resume: Creating Controllers for authentication class</para>
+    /// <para>Created by: Joceline Gutierrez</para>
+    /// <para>Version: 1.0</para>
+    /// <para>Date: 12/05/2022</para>
+    /// </summary>
+
+
+    [ApiController]
+    [Route("api/Authentication")]
+    [Produces("application/json")]
+    public class AuthenticationController : ControllerBase
+    {
+        #region Attributes
+        private readonly IAuthentication _services;
+        #endregion
+
+        #region Constructors
+        public AuthenticationController(IAuthentication services)
+        {
+            _services = services;
+        }
+        #endregion
+
+        #region Mhetods
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<ActionResult> AuthenticationAsync([FromBody] AuthenticationDTO authentication)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            try
+            {
+                var authorization = await _services.GetAuthorizationAsync(authentication);
+                return Ok(authorization);
+            }
+            catch (Exception ex)
+            {
+                return Unauthorized(ex.Message);
+            }
+        }
+        #endregion
+    }
+}
